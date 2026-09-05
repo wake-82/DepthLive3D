7d578840878f818a5679ae673d6dfd44bc993408
wake-82 <amadorei9@gmail.com>
2026-09-05 10:29:30 +0900

    Update live3d.py

--- live3d.py
+++ live3d.py
@@ -128,13 +128,13 @@ _EDGE_FIX_LEVEL_MAP = {
 }
 
 # VDA-specific edge-fix level -> (x, y) mapping.
-# level 1: x=4,y=0 / level 2: x=5,y=1 / level 3: x=6,y=2 / level 4: x=7,y=2 / level 5: x=9,y=2
+# level 1: x=4,y=0 / level 2: x=5,y=1 / level 3: x=6,y=2 / level 4: x=8,y=2 / level 5: x=9,y=2
 _EDGE_FIX_LEVEL_MAP_VDA = {
     0: (0, 0),
     1: (4, 0),
     2: (5, 1),
     3: (6, 2),
-    4: (7, 2),
+    4: (8, 2),
     5: (9, 2),
 }
 
